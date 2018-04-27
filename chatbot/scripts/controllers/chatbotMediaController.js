bfa24f73e00e85ca604b73c57ac3d7972cf8c126
Divani Iranga <dvairanga@gmail.com>
2018-04-27 17:10:10 +0530

    filter a type

--- chatbot/scripts/controllers/chatbotMediaController.js
+++ chatbot/scripts/controllers/chatbotMediaController.js
@@ -9,6 +9,18 @@ mainApp.controller('chatbotMediaController', function ($scope, $q, $anchorScroll
             console.log(response);
             if (response.data.IsSuccess) {
                 $scope.allmediaFiles = response.data.Result;
+
+                var ty = $scope.allmediaFiles.reduce(function (a, b) {
+                    if (a.indexOf(b.type) == -1) {
+                      a.push(b.type)
+                    }
+                    return a;
+                  }, []);
+                  
+               
+                  $scope.mediaTypes=[];
+                  $scope.mediaTypes = ty;
+
             } else {
                 $scope.showAlert("ChatBot", 'error', "Fail To load Media.");
             }
@@ -26,13 +38,23 @@ mainApp.controller('chatbotMediaController', function ($scope, $q, $anchorScroll
         $state.reload();
     };
     $scope.val = 'created_at';
-    $scope.sort = 'created_at'
+    $scope.sort = 'created_at';
     $scope.reverse = false;
-   $scope.sorting = function(val){
-       console.log(val);
-    $scope.reverse = ($scope.val === val) ? !$scope.reverse : false;
-    $scope.val = val;
-   }
 
+    $scope.sorting = function(val){
+        console.log(val);
+        $scope.reverse = ($scope.val === val) ? !$scope.reverse : false;
+        $scope.val = val;
+    }
+   
+    $scope.filterType = function(type){
+        console.log(type);
+        $scope.selectedtype = type;
+    }
+
+    $scope.resetFilters = function(){
+        $scope.selectedtype = "";
+        $scope.type = "";
+    }
 
 });
