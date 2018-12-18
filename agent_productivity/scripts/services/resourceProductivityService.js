e34c2c1516f0b6f5c9d3d53ef7c92ba925a29508
waruna <waruna@duosoftware.com>
2018-12-18 13:58:41 +0530

    send bu with productivity request

--- agent_productivity/scripts/services/resourceProductivityService.js
+++ agent_productivity/scripts/services/resourceProductivityService.js
@@ -6,15 +6,22 @@ var clusterModule = angular.module("resourceProductivityServiceModule", []);
 
 clusterModule.factory("resourceProductivityService", function ($http, $log, authService, baseUrls) {
 
+    var postdata = {bu:ShareData.BusinessUnit};
+    if(ShareData.BusinessUnit.toLowerCase()==="all"){
+        postdata = {};
+    }
     var getProductivity = function () {
-
-        return $http.get(baseUrls.resourceServiceBaseUrl + "Resources/Productivity").then(function (response) {
-                if (response.data && response.data.IsSuccess) {
-                    return response.data.Result;
-                } else {
-                    return {};
-                }
-            });
+        return $http({
+            method: 'get',
+            url: baseUrls.resourceServiceBaseUrl + "Resources/Productivity",
+            params:postdata
+        }).then(function (response) {
+            if (response.data && response.data.IsSuccess) {
+                return response.data.Result;
+            } else {
+                return {};
+            }
+        });
     };
 
     var getOnlineAgents = function () {
