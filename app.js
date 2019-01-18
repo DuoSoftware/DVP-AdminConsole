42f9d6403da4533d228e5d61971fcd1b621d9313
Pawan Sasanka <pawan@duosoftware.com>
2019-01-18 12:39:43 +0530

    updated

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ var baseUrls = {
     'dashBordUrl': 'http://dashboardservice.app1.veery.cloud/',
     'autoattendantUrl': 'http://autoattendant.app1.veery.cloud/DVP/API/1.0.0.0/',
     'TrunkServiceURL': 'http://phonenumbertrunkservice.app1.veery.cloud/DVP/API/1.0.0.0/',
-    'socialConnectorUrl': 'http://socialconnector.app.veery.cloud/DVP/API/1.0.0.0/Social/', //104.236.197.119
+    'socialConnectorUrl': ' https://f70b2d28.ngrok.io/DVP/API/1.0.0.0/Social/', //104.236.197.119
     'mailSenderUrl': 'http://mailsender.app.veery.cloud/DVP/API/1.0.0.0/Social/', //104.236.197.119
     'notification': 'http://notificationservice.app1.veery.cloud/',//notificationservice.app1.veery.cloud
     'cdrProcessor': 'http://cdrprocessor.app1.veery.cloud/DVP/API/1.0.0.0/CallCDR/', //cdrprocessor.app1.veery.cloud
