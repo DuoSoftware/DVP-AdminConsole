d90132a6f23d2cc111812b22ea72fb4ca9837cbf
marlonabeykoon <marlonabeykoon@gmail.com>
2019-01-24 11:35:32 +0530

    hourly band rep navigation added

--- app.js
+++ app.js
@@ -814,7 +814,7 @@ mainApp.config(["$httpProvider", "$stateProvider", "$urlRouterProvider", "$authP
             controller: "hourlyBandReportCtrl",
             data: {
                 requireLogin: true,
-                navigation: "QUEUE_HOURLY_BREAKDOWN_REPORT"
+                navigation: "HOURLY_BAND_REPORT"
             }
         }).state('console.sla', {
             url: "/sla",
