f0346359d0586126718476315671026666418ee2
Pawan Sasanka <pawan@duosoftware.com>
2019-05-15 16:17:32 +0530

    warning message updated on login

--- auth/controller/loginCtrl.js
+++ auth/controller/loginCtrl.js
@@ -165,7 +165,8 @@ mainApp.controller('loginCtrl', function ($rootScope, $scope, $state, $http,
             .catch(function (error) {
                 if (error.status == 449 || error.status == 401) {
 					if (error.data.message.toLowerCase() == 'invalid user account') {
-						showAlert('Account Info', 'warning', error.data.message + '. Make sure you have activated your account through the email you may have received');
+						//showAlert('Account Info', 'warning', error.data.message + '. Make sure you have activated your account through the email you may have received');
+                        showAlert('Account Info', 'warning','Problem in Login, Mack sure you have entered correct credentials or correct login method ')
 					}else{
 						showAlert('Account Info', 'warning', error.data.message);
 					}
