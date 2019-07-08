d8900869bc7696d6c18494c14160587eaa2b188c
Nimeshka Srimal <nimeshka@gmail.com>
2019-07-08 17:37:16 +0530

    Add agent drop down in chat report

--- controller/chatReport/chatReportCtrl.js
+++ controller/chatReport/chatReportCtrl.js
@@ -1,4 +1,4 @@
-mainApp.controller("chatReportCtrl", function ($scope, $uibModal, interactionService, mailService, loginService) {
+mainApp.controller("chatReportCtrl", function ($scope, $uibModal, interactionService, mailService, loginService, $q, ShareData) {
 
     $scope.showAlert = function (tittle, type, content) {
 
@@ -31,6 +31,7 @@ mainApp.controller("chatReportCtrl", function ($scope, $uibModal, interactionSer
         limit: '10'
     };
 
+    $scope.Agents = [];
     $scope.chatSessions = [];
 
     $scope.search = function () {
@@ -135,5 +136,70 @@ mainApp.controller("chatReportCtrl", function ($scope, $uibModal, interactionSer
             $scope.showAlert('Chat Report', 'error', 'Error occurred while loading messages');
         });
     };
-    
+
+    // following code block taken from agent summary control.
+    $scope.getAgents = function () {
+
+        ShareData.getAgentDetailsCount().then(function (row_count) {
+            var pagesize = 20;
+            var pagecount = Math.ceil(row_count / pagesize);
+
+            var method_list = [];
+
+            for (var i = 1; i <= pagecount; i++) {
+                method_list.push(ShareData.getAgentDetailsWithPaging(pagesize, i));
+            }
+
+            $q.all(method_list).then(function (resolveData) {
+                if (resolveData) {
+                    resolveData.map(function (data) {
+                        data.map(function (item) {
+                            $scope.Agents.push(item);
+                        });
+                    });
+                } else {
+                    $scope.showAlert("Error","Error in loading agent details","error");
+                }
+            }).catch(function (err) {
+                console.log("Error in Agent details picking " + err);
+
+                $scope.showAlert("Error","Error in loading Agent details","error");
+            });
+        }, function (err) {
+            console.log("Error in Agent details picking " + err);
+            $scope.showAlert("Error","Error in loading Agent details","error");
+        });
+    };
+
+    $scope.getAgents();
+
+    $scope.querySearch = function (query) {
+        var emptyArr = [];
+        if (query === "*" || query === "") {
+            if ($scope.Agents) {
+                return $scope.Agents;
+            } else {
+                return emptyArr;
+            }
+
+        } else {
+            if ($scope.Agents) {
+                var filteredArr = $scope.Agents.filter(function (item) {
+
+                    if (item.ResourceName) {
+                        return item.ResourceName.toString().toLowerCase().match(query);
+                    }
+                    else {
+                        return false;
+                    }
+
+                });
+
+                return filteredArr;
+            } else {
+                return emptyArr;
+            }
+        }
+
+    };
 });
